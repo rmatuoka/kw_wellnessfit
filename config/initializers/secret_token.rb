c8f3746f8f45d18d53bc4ddda718146502d1dc44
-_- Zaba -_- <bsilva@korewa.com.br>
2011-08-17 16:11:09 -0300

    Inicio do Repositorio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KwWellnessfit::Application.config.secret_token = '13b371f86e7415181a1c1a604f1af0c5471f0aec36aaadba311a0a7c3ed96f70f587fed6c7e48d4be7ee08f8189c8478bb7680a596634bc1bab43f1a38dabf0a'

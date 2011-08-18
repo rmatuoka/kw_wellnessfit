6a0d26a2492cca049f1cd5c8b17116b1b4034243
Cristiano Nascimento <cnascimento@korewa.com.br>
2011-08-18 14:25:21 -0700

    home sem menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 KwWellnessfit::Application.routes.draw do
-  get "ciafisio/index"
-
-  get "ciafisio/show"
-
-  get "about/index"
 
  resources :home
+ resources :ciafisio
+ resources :about
  
  root :to => "home#index"
 end

2aa49f49c3ecc3503d2bb135fc7368594e296cec
Cristiano Nascimento <cnascimento@korewa.com.br>
2011-09-29 10:44:52 -0700

    alterações mailer , e internas

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -6,7 +6,7 @@ class ContactMailer < ActionMailer::Base
       @Email = email
       @Telefone = telefone
       @Msg = msg
-      mail(:to => "felipe@wellnessfit.com.br",:bcc => "rmatuoka@korewa.com.br", :subject => "Wellnes - Contato via site")
+      mail(:to => "contato@wellnessfit.com.br",:bcc => "rmatuoka@korewa.com.br", :subject => "Wellnes - Contato via site")
       #mail(:to => @Contato.area, :bbc => @Contato.area, :subject => "Nautilus - Contato via site")
   end
 
@@ -15,7 +15,7 @@ class ContactMailer < ActionMailer::Base
       @Email = email
       @Telefone = telefone
       attachments[anexado] = File.read(anexado)
-      mail(:to => "cnascimento@korewa.com.br",:bcc => "cnascimento@korewa.com.br", :subject => "Wellnes - Curriculo via Site")
-      #mail(:to => "felipe@wellnessfit.com.br",:bcc => "rmatuoka@korewa.com.br", :subject => "Wellnes - Curriculo via Site")
+      mail(:to => "contato@wellnessfit.com.br",:bcc => "rmatuoka@korewa.com.br", :subject => "Wellnes - Curriculo via Site")
+      #mail(:to => "cnascimento@korewa.com.br",:bcc => "cnascimento@korewa.com.br", :subject => "Wellnes - Curriculo via Site")
   end
 end
